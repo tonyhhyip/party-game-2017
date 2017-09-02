f181d234809a720366458ccb9a8f0cf2988daf61
Tony Yip <tony@opensource.hk>
2017-09-03 02:19:00 +0800

    Add name field for attendees

--- src/backend/graphql/resolvers/query/attendees.js
+++ src/backend/graphql/resolvers/query/attendees.js
@@ -1,9 +1,10 @@
 module.exports = (root, args, context) => {
-  const query = 'SELECT id, checkIn, finishGame FROM attendees';
+  const query = 'SELECT id, checkIn, finishGame, name FROM attendees';
   return context.db.any(query)
     .then(rows => rows.map(row => ({
       id: row.id,
       finishGame: row.finishgame,
       checkIn: row.checkin,
+      name: row.name,
     })));
 };
