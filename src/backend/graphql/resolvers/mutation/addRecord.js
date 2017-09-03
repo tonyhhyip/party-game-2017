45245ce2015706b76995a178bc39cab607de9fa0
Tony Yip <tony@opensource.hk>
2017-09-04 03:37:53 +0800

    Add check for attendee check in

--- src/backend/graphql/resolvers/mutation/addRecord.js
+++ src/backend/graphql/resolvers/mutation/addRecord.js
@@ -1,10 +1,19 @@
 module.exports = (obj, { attendee, booth }, context) => {
-  const sql = `
+  const insertQuery = `
   INSERT INTO records (attendee, booth) VALUES ($1, $2)
   ON CONFLICT (attendee, booth) DO UPDATE SET create_time = NOW()
   RETURNING create_time
   `;
-  return context.db.one(sql, [attendee.id, booth.id])
+  const selectQuery = `
+  SELECT 1 FROM attendees WHERE id = $1 AND checkIn IS NOT NULL
+  `;
+  return context.db.tx(t => t.result(selectQuery, [attendee.id])
+    .then((result) => {
+      if (result.rowCount === 0) {
+        throw new Error('Require Check In');
+      }
+    })
+    .then(() => t.one(insertQuery, [attendee.id, booth.id])))
     .then(data => ({
       createTime: data.create_time,
       attendee: { id: attendee.id },
