782c825e9c78969d389bb23575089d03c723b937
Tony Yip <tony@opensource.hk>
2017-09-04 01:56:35 +0800

    Add records for attendees

---
+++ src/backend/graphql/resolvers/attendee.js
@@ -0,0 +1,22 @@
+module.exports = {
+  records(obj, args, context) {
+    const sql = `
+    SELECT booth.id, booth.name, booth.token, records,create_time FROM booth
+    LEFT JOIN records ON booth.id = records.booth
+    WHERE records.attendee IS NULL OR records.attendee = $1  
+  `;
+    return context.db.any(sql, [obj.id])
+      .then((rows) => {
+        console.log(rows);
+        return rows.map(row => ({
+          createTime: row.create_time,
+          booth: {
+            id: row.id,
+            name: row.name,
+            token: row.token,
+          },
+          attendee: obj,
+        }));
+      });
+  }
+};
