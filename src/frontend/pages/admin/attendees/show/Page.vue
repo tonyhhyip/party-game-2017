26d888f36aef6083d4ba9a62222bdbb8b4aae981
Tony Yip <tony@opensource.hk>
2017-09-04 02:08:35 +0800

    Display record of attendees

--- src/frontend/pages/admin/attendees/show/Page.vue
+++ src/frontend/pages/admin/attendees/show/Page.vue
@@ -7,22 +7,42 @@
       </v-flex>
     </v-layout>
 
-    <v-layout row v-if="attendee != null">
-      <v-flex xs12>
-        <v-card>
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline">{{ attendee.name }}</h3>
-              <div>Check In: {{ attendee.checkIn | time }}</div>
-            </div>
-          </v-card-title>
+    <template v-if="attendee != null">
+      <v-layout row>
+        <v-flex xs12>
+          <v-card>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline">{{ attendee.name }}</h3>
+                <div>Check In: {{ attendee.checkIn | time }}</div>
+              </div>
+            </v-card-title>
 
-          <v-card-actions>
-            <v-btn flat @click="checkIn" :disabled="attendee.checkIn !== null">Check In</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
+            <v-card-actions>
+              <v-btn flat @click="checkIn" :disabled="attendee.checkIn !== null">Check In</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+      <v-layout row>
+        <v-flex xs12>
+          <v-list two-line subheader>
+            <v-subheader inset>Booth Game</v-subheader>
+            <v-list-tile avatar v-for="record in attendee.records" :key="record.booth.name">
+              <v-list-tile-avatar>
+                <v-icon :class="record.createTime === null ? 'red--text' : 'green--text'" class="grey lighten-2">
+                  {{ record.createTime === null ? 'close' : 'done' }}
+                </v-icon>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ record.booth.name }}</v-list-tile-title>
+                <v-list-tile-sub-title>{{ record.createTime | time }}</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-flex>
+      </v-layout>
+    </template>
     <v-progress-circular indeterminate :size="70" class="primary--text" v-else></v-progress-circular>
 
     <v-snackbar :timeout="5000" top v-model="snackbar">
