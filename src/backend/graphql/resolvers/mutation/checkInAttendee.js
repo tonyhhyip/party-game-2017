b29715558c864e7d4feb1d3c6599aeaef39bbaab
Tony Yip <tony@opensource.hk>
2017-09-04 23:08:54 +0800

    Please eslint

--- src/backend/graphql/resolvers/mutation/checkInAttendee.js
+++ src/backend/graphql/resolvers/mutation/checkInAttendee.js
@@ -1,7 +1,7 @@
 module.exports = (root, { attendee }, context) => {
   const { id } = attendee;
   const updateQuery = 'UPDATE attendees SET check_in = NOW() WHERE id = $1 RETURNING id, name, check_in, finish_game';
-  return context.db.task(t => t.tx(t => t.one(updateQuery, [id])))
+  return context.db.task(h => h.tx(t => t.one(updateQuery, [id])))
     .then(row => ({
       id: row.id,
       finishGame: row.finish_game,
