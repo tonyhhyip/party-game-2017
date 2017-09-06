297579d47ca4698deef020b8b419615a58a12f3a
Tony Yip <tony@opensource.hk>
2017-09-06 15:13:30 +0800

    Fix wrong query

--- src/backend/webhook.js
+++ src/backend/webhook.js
@@ -7,18 +7,14 @@ const router = Router();
 router.post(`/webhook/eventbrite/${process.env.EVENTBRITE_EVENT_ID}`, (req, resp) => {
   const path = url.parse(req.body.api_url).pathname;
   const pieces = path.replace(/^\/|\/$/, '').split('/');
-  const id = pieces.reduce((val, piece) => {
-    return val === null ? piece : val;
-  }, null);
+  const id = pieces.reduce((val, piece) => (val === null ? piece : val), null);
   const db = pg();
-  const sql = 'UPDATE attendees SET check_in = NOW WHERE id = $1';
+  const sql = 'UPDATE attendees SET check_in = NOW() WHERE id = $1';
   db.task(h => h.tx(t => t.none(sql, [id])))
     .then(() => { resp.status(200).end(); });
 });
 
-module.exports = (app) => {
-  return new Promise((resolve) => {
-    app.use(router);
-    resolve(app);
-  });
-};
+module.exports = app => new Promise((resolve) => {
+  app.use(router);
+  resolve(app);
+});
