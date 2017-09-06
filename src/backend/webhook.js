bf297cec5504d263413398eff44e774c1a7a93ae
Tony Yip <tony@opensource.hk>
2017-09-06 16:02:49 +0800

    Fix webhook issue

--- src/backend/webhook.js
+++ src/backend/webhook.js
@@ -7,11 +7,14 @@ const router = Router();
 router.post(`/webhook/eventbrite/${process.env.EVENTBRITE_EVENT_ID}`, (req, resp) => {
   const path = url.parse(req.body.api_url).pathname;
   const pieces = path.replace(/^\/|\/$/, '').split('/');
-  const id = pieces.reduce((val, piece) => (val === null ? piece : val), null);
+  let id = null;
+  while (!id || id === null) {
+    id = pieces.pop();
+  }
   const db = pg();
   const sql = 'UPDATE attendees SET check_in = NOW() WHERE id = $1';
-  db.task(h => h.tx(t => t.none(sql, [id])))
-    .then(() => { resp.status(200).end(); });
+  db.task(h => h.tx(t => t.result(sql, [id])))
+    .then(({ rowCount }) => { resp.status(rowCount === 1 ? 200 : 500).end(); });
 });
 
 module.exports = app => new Promise((resolve) => {
