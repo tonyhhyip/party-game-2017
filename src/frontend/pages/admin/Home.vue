ea877d0357c1be2bb3b304ec8d2efb4b2372710b
Tony Yip <tony@opensource.hk>
2017-09-27 18:46:04 +0800

    Fix typo of elements

--- src/frontend/pages/admin/Home.vue
+++ src/frontend/pages/admin/Home.vue
@@ -8,17 +8,17 @@
         <v-list-tile-avatar>
           <v-icon class="grey lighten-2">people</v-icon>
         </v-list-tile-avatar>
-        <v-list-title-content>
+        <v-list-tile-content>
           Attendee
-        </v-list-title-content>
+        </v-list-tile-content>
       </v-list-tile>
       <v-list-tile avatar to="/admin/booth">
         <v-list-tile-avatar>
           <v-icon class="grey lighten-2">business</v-icon>
         </v-list-tile-avatar>
-        <v-list-title-content>
+        <v-list-tile-content>
           Booth
-        </v-list-title-content>
+        </v-list-tile-content>
       </v-list-tile>
     </v-list>
   </v-layout>
